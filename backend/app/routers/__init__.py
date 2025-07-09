768407227c84516fc7f2f6cbf278c23bde24d9f4
famkri <i@famkri.com>
2025-07-09 18:32:35 +0200

    Delete backend/app/routers directory

--- backend/app/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from .auth import router as auth_router
-from .listings import router as listings_router
-
-api_router = APIRouter()
-
-# Include routers with prefixes
-api_router.include_router(auth_router, tags=["authentication"])
-api_router.include_router(listings_router, tags=["listings"])
